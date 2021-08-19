b9f3f0bb75e68b1f978b48e8799dd266d3fbd45b
zeedtanue <zeedtanue@gmail.com>
2021-08-19 16:16:36 +0600

    mongodb deprication warning

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true)
 mongoose.set( 'useNewUrlParser', true )
+mongoose.set( 'useFindAndModify', false)
 
 
 mongoose.connect(process.env.MONGO_URI,(err)=>{
