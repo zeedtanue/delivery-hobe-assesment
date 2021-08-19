40aaf7576637f1c5f53635aa721b78e044d284e3
zeedtanue <zeedtanue@gmail.com>
2021-08-19 21:07:02 +0600

    warehouse create get

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const { postWarehouse, getAllWarehouses, getWarehouse } = require('../controllers/warehouse');
 var router = express.Router();
 
 
-router.get('/', (req,res)=>{
-    res.status(200).json('works')
-});
+router.get('/', getAllWarehouses);
+router.get('/:id', getWarehouse)
+router.post('/new', postWarehouse);
 
 module.exports = router;
