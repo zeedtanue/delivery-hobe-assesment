1f3149291d2bd3c37f7f8fe871d99da2f6ba9e99
zeedtanue <zeedtanue@gmail.com>
2021-08-19 18:05:28 +0600

    product get and delete

--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 
-const { postNewProduct, getProducts, updateProduct } = require('../controllers/products');
+const { postNewProduct,
+        getProducts,
+        updateProduct, 
+        deleteProduct, 
+        getOneProduct } = require('../controllers/products');
 
 
 router.get('/', getProducts);
 
 
 router.post('/new', postNewProduct)
-
+router.get('/:id', getOneProduct)
 router.put('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 
 module.exports = router;
